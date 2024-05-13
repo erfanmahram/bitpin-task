c8cb4f086773768ae0f7e62cfda8fddf203d8027
Erfan Mahram <e.mahram@devopsyar.com>
2024-05-13 19:26:26 +0330

    Add docstring for models

--- blog/models.py
+++ blog/models.py
@@ -3,21 +3,31 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+    """
+    This model stores a post instance
+
+    Required Fields:
+    title: <str>
+    content: <str>
+
+    """
+
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def average_rating(self):
-        ratings = self.ratings.all()
-        if ratings:
-            return sum(rating.rating for rating in ratings) / len(ratings)
-        return 0
-
-    def num_ratings(self):
-        return self.ratings.count()
-
 
 class Rating(models.Model):
+    """
+    This model stores a Rating instance
+
+    Required Fields:
+    user: <user> --> pk of the user model
+    post: <post> --> pk of the post model
+    rating: <int>
+
+    """
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, related_name='ratings', on_delete=models.CASCADE)
     rating = models.IntegerField(choices=[(i, str(i)) for i in range(6)])
